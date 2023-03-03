cb97724d99988f2f0a3865fe7bfe039acea48623
Ali Bailoun <alibailoun1982@gmail.com>
2023-03-03 01:29:36 -0500

    GET, create, delete and update thought controller done

---
+++ controllers/thoughtController.js
@@ -0,0 +1,50 @@
+const { Thought, User } = require("../models");
+
+
+module.exports = {
+    getThoughts(req, res) {
+        Thought.find()
+            .then((thoughts) => res.json(thoughts))
+            .catch((err) => res.status(500).json(err));
+    },
+    getOneThought(req, res) {
+        Thought.findOne({ _id: req.params.thoughtId })
+            .select('-__v')
+            .then((thought) =>
+                !thought
+                    ? res.status(404).json({ message: "There is no thought associated with that ID" })
+                    : res.json(thought)
+            )
+            .catch((err) => re.status(500).json(err));
+    },
+    createThought(req, res) {
+        Thought.create(req.body)
+            .then((thought) => res.json(thought))
+            .catch((err) => {
+                console.log(err);
+                return res.status(500).json(err);
+            });
+    },
+    deleteThought(req, res) {
+        Thought.findOneAndDelete({ _id: req.params.thoughtId })
+            .then((thought) =>
+                !thought
+                    ? res.status(404).json({ message: "There is no thought associated with that ID" })
+                    : res.json({ message: "Thought successfully deleted" })
+            )
+            .catch((err) => res.status(500).json(err));
+    },
+    updateThought(req, res) {
+        Thought.findOneAndUpdate({ _id: req.params.thoughtId },
+            { $set: rq.body },
+            { runValidators: true, new: true }
+        )
+            .then((thought) =>
+                !thought
+                    ? res.status(404).json({ message: "There is no thought associated with that ID" })
+                    : res.json(thought)
+            )
+            .catch((err) =>
+                res.status(500).json(err));
+    }
+}
