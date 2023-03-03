e6470654021a25067dbaf873969942b4a43a2eb6
Ali Bailoun <alibailoun1982@gmail.com>
2023-03-02 23:33:37 -0500

    all schemas and models are done

---
+++ models/Thought.js
@@ -0,0 +1,87 @@
+const { Schema, model } = require('mongoose')
+
+
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            maxLength: 280,
+            minLength: 1
+        }
+    },
+    {
+        createdAt: {
+            type: Date,
+            default: Date.now
+        }
+    },
+    {
+        username: {
+            type: String,
+            required: true
+        }
+    },
+    {
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+
+    }
+);
+
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: Date.now
+        }
+    },
+    {
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280
+        }
+    },
+    {
+        username: {
+            type: String,
+            required: true
+        }
+    },
+    {
+        createdAt: {
+            type: Date,
+            default: Date.now
+        }
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        }
+    }
+)
+
+
+reactionSchema.virtual('createdStamp').get(function () {
+    return new Date.toLocaleDateString()
+})
+
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', thoughtSchema)
+
+
+const timeCreated = Thought.get('createdAt', Date);
+
+console.log(`The thought was created at ${timeCreated}`)
--- models/User.js
+++ models/User.js
@@ -38,3 +38,12 @@ const userSchema = new Schema(
         id: false
     }
 );
+
+
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+const User = model('user', userSchema);
+
+module.exports = User;
