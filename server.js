a308d2216cb4efc4f7db2471aeed7e350f9abf46
Ali Bailoun <alibailoun1982@gmail.com>
2023-03-06 18:11:23 -0500

    fixed routes and finalised work

--- server.js
+++ server.js
@@ -5,9 +5,13 @@ const routes = require("./routes");
 const PORT = 3001;
 const app = express();
 
+
 app.use(express.urlencoded({ extended: true }));
+
 app.use(express.json());
+
 app.use(routes);
+
 db.once("open", () => {
     app.listen(PORT, () => {
         console.log(`App is listening on port ${PORT}!`);
